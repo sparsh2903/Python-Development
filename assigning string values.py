3dd8b947a302f6ca64c032f313af9152be604c52
sparsh2903 <85448284+sparsh2903@users.noreply.github.com>
2022-10-02 16:08:28 +0530

    Add files via upload
    played with strings via python :-)

---
+++ assigning string values.py
@@ -0,0 +1,14 @@
+
+# woking with strings 
+
+favLang = {}
+a = input("what is your favourite language sparsh\n")
+b = input("what is your favourite language mayank\n")
+c = input("what is your favourite language harsh\n")
+d = input("what is your favourite language aditya\n")
+favLang['sparsh'] = a
+favLang['mayank'] = b
+favLang['harsh'] = c
+favLang['aditya'] = d
+
+print(favLang)
