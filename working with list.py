d09763d4ee2f7b08a4aaa4076682b3e271386d89
sparsh2903 <85448284+sparsh2903@users.noreply.github.com>
2022-10-09 19:57:13 +0530

    Add files via upload
    tried some "List" functions...

---
+++ working with list.py
@@ -0,0 +1,10 @@
+
+# working with list 
+
+names = ["sparsh", "vidhi", "aditya", "dhruv", "mayank", "harsh"]
+name = input("Enter the name to check\n")
+
+if name in names:
+    print("the name you typed is present in the list")
+else:
+    print("The name you typed is not present in the list")
